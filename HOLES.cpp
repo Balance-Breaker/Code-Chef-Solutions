9bf55c303800f499c3628d2e2936a36800009260
Ankit Dhankhar <ankit97dhankhar@gmail.com>
2017-05-21 22:42:26 +0530

    Update HOLES.cpp

--- HOLES.cpp
+++ HOLES.cpp
@@ -2,27 +2,25 @@
 using namespace std;
 int main(){
 	int testcases;
-	scanf("%d",&testcases);
-	while(testcases--){
-		string str;
-		int res=0;
-		cin>>str;
-		for(int i=0;i<str.length();i++){
-			switch(str(i)){
-				case 'A':res++;
-				case 'D':
-				case 'O':
-				case 'P':
-				case 'R':
-				case 'Q':res++;
-						break;
-				case 'B':res+=2;
-				break;
-				
-				default :break;
-			}
-			printf("%d\n",res );
-		}
-	}
-	return 0;
-}
+  	scanf("%d",&testcases);
+  	while(testcases--){
+    	string temp;
+    	int res=0;
+    	cin>>temp;
+    	for(int i=0;i<temp.length();i++){
+    		switch(temp.at(i)){
+    			case 'A':
+    			case 'D':
+    			case 'O':
+    			case 'P':
+    			case 'R':
+    			case 'Q':res++;
+    					break;
+    			case 'B':res+=2;
+    				break;
+    			default :break;
+    			}
+    		}
+   		printf("%d\n",res );
+   	}
+} 
