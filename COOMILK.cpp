69b27a1ef80539525ffc35154b075e5379831dd6
Ankit Dhankhar <ankit97dhankhar@gmail.com>
2017-05-21 22:41:27 +0530

    Update COOMILK.cpp

--- COOMILK.cpp
+++ COOMILK.cpp
@@ -2,29 +2,27 @@
 using namespace std;
 int main()
 {
-	int testcases;
-	scanf("%d",&testcases);
-	while(testcases--)
-	{
-		int n;
-		scanf("%d",&n);
-		bool flag=true;
-		string str[n];
-		for (int i = 0; i < n; ++i)
-		{
-			scanf("%s",&str[i]);
-		}
-		for (int i = 0; i < n; ++i)
-		{	if(str[i]=="cookie")
-				if(i==n-1|| s[i+1]!="milk")
-					flag=false;
-		
-		
-		}
-		if(flag)
-			printf("YES\n");
-		else
-			printf("NO\n");
-	}
-	return 0;
-}
+  	int testcases;
+   	scanf("%d",&testcases);
+   	while(testcases--)
+   	{
+   		int n;
+  		scanf("%d",&n);
+   		bool flag=true;
+   		string str[n];
+   		for (int i = 0; i < n; ++i)
+   		{
+   			cin>>str[i];
+   		}
+   		for (int i = 0; i < n; ++i)
+   		{	if(str[i]=="cookie")
+   				if(i==n-1|| str[i+1]!="milk")
+   					flag=false;
+   		}
+   		if(flag)
+   			printf("YES\n");
+   		else
+   			printf("NO\n");
+   	}
+   	return 0;
+    } 
