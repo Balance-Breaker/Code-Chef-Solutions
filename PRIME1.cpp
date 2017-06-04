c3ac8b941b4192dee0472328a5d2b17f212d1c5a
Ankit Dhankhar <ankit97dhankhar@gmail.com>
2017-06-04 23:10:03 +0530

    Update PRIME1.cpp

--- PRIME1.cpp
+++ PRIME1.cpp
@@ -1,34 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int isprm(long long int n)
-{
-    long long int i=5, w=2;
-    if(n==2 || n== 3)
-     return 1;
-     if(n%2==0 || n%3==0 || n==1)
-        return 0;
-     while(i*i<= n)
-     {
-         if(n%i ==0)
-            return 0;
-            i +=w;
-            w=6-w;
-     }
-     return 1;
-}
-
-int main()
-{   int testcases;
-    scanf("%d",&testcases);
-    while(testcases--){
-    int i,j;
-    scanf("%d%d",&i,&j);
-    for(int i; i<j;i++)
-    {
-        if(isprm(i)==1)
-            printf("%d\n",i);
+    #include<bits/stdc++.h>
+    using namespace std;
+    bool sieve[100018]={0};
+    vector<int> primes;
+     
+    void pre(){
+    	for(int i=2;i<(100018);i++){
+    		if(!sieve[i]){
+    			primes.push_back(i);
+    			for(int j=2*i;j<100018;j+=i) sieve[j]=1;
+    		}
+    	}
     }
-}
-    return 0;
-}
+        
+    int main(){    	
+    	ios_base::sync_with_stdio(0);
+    	cin.tie(0);
+    	cout.tie(0);
+     	int t;
+     	pre();
+     	cin>>t;
+     	while(t--){
+     		int n,m;
+     		cin>>m>>n;
+     		bool check[n-m+1]={0};
+     		for(int i=0;i<primes.size()&&primes[i]<=n;i++){
+     			int start;
+     			if(m%primes[i]) start=primes[i]*((m/primes[i])+1);
+     			else start=m;
+     			for(int j=start-m;j<=n-m;j+=primes[i]){
+     				if(j+m!=primes[i])check[j]=1;
+     			}
+     		}
+     		for(int i=0;i<sizeof(check)/sizeof(bool);i++){
+     			if(!check[i]&&m+i!=1) cout<<m+i<<endl;
+     		}
+     		cout<<endl;
+     	}
+       	return 0;
+    } 
