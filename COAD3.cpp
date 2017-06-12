8c15c8263328ac1c5d4d979dab750ef2895cf56b
Ankit Dhankhar <ankit97dhankhar@gmail.com>
2017-06-12 22:28:57 +0530

    Update COAD3.cpp

--- COAD3.cpp
+++ COAD3.cpp
@@ -1,1 +1,19 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int ar[1001]={0};
+    for(int i=1;i<=1000;i++){
+        ar[i]=ar[i-1]+1;
+        if(i%2==0){
+            ar[i]=min(ar[i],ar[i/2]+2);
+        }
+    }
+    int testcases;
+    cin>>testcases;
+    while(testcases--){
+        int n;
+        cin>>n;
+        cout<<ar[n]<<endl;
+    }
+    return 0;
+}
